127e0d0ccc45ca561323f073c1cb4172531934e2
Artem Kraievyi <artem.kraievyi@gmail.com>
2023-12-04 20:44:35 +0200

    feat: add get_context_data for view render

--- food_consuming/views.py
+++ food_consuming/views.py
@@ -3,5 +3,14 @@ from food_consuming.models import Food, Consume
 from django.http import HttpResponse
 
 
-def say_hello(request):
-    return HttpResponse("Hello World")
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['foods'] = Food.objects.all()
+        context['consumed_food'] = Consume.objects.filter(user=self.request.user)
+        return context
