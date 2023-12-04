5505c31e619c7a3bdb80b89eb3343729915a5c92
Artem Kraievyi <artem.kraievyi@gmail.com>
2023-12-04 20:24:08 +0200

    feat: create Food and foodConsumed models

---
+++ food_consuming/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    carbs = models.FloatField()
+    proteins = models.FloatField()
+    fats = models.FloatField()
+    calories = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class FoodConsumed(models.Model):
+    user = models.ForeignKey(User)
+    food = models.ForeignKey(User)
+
+
+
+
+
